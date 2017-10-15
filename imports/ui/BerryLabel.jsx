d3d8448028d96f09bea2761e4c3e7963b4c16f0f
Henri Kajava <henri.kajava@gmail.com>
2017-10-15 20:45:18 +0300

    modify image source info formatting

--- imports/ui/BerryLabel.jsx
+++ imports/ui/BerryLabel.jsx
@@ -5,12 +5,14 @@ export default class BerryLabel extends Component {
   render() {
     if (this.props.answerGiven) {
       return (
+        <div className='content-area-container'>
           <ul>
             <li>{this.props.berryName} ({this.props.berryNameInFinnish})</li>
             <li>Source:  <a target="_blank" rel="noopener noreferrer"
               href={this.props.wikiURL}>Wikipedia</a></li>
             <li>Author: {this.props.authorName}</li>
           </ul>
+        </div>
       );
     }
     return (null);
